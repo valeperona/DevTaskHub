88617fc692e11bf4cb1803b413db6035dc213617
Valentina Perona <2112356@ucc.edu.ar>
2025-11-14 12:27:07 -0300

    cypress fix

--- frontend/devtaskhub-frontend/cypress.config.ts
+++ frontend/devtaskhub-frontend/cypress.config.ts
@@ -1,10 +1,25 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
+import { createViteBundler } from '@cypress/vite-dev-server';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:5173", // puerto de Vite
+    baseUrl: 'http://localhost:5173',
+    specPattern: 'cypress/e2e/**/*.cy.ts',
+    supportFile: 'cypress/support/e2e.ts',
     viewportWidth: 1280,
     viewportHeight: 720,
     video: false,
-  },
+    setupNodeEvents(on, config) {
+      on('dev-server:start', async (options) => {
+        return createViteBundler({
+          config: {
+            plugins: [react()]
+          }
+        })(options);
+      });
+
+      return config;
+    }
+  }
 });
