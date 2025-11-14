24203dbf38d04b50a010bcdcbab4e29eed2134f0
Valentina Perona <2112356@ucc.edu.ar>
2025-11-14 02:50:30 -0300

    fix pipeline

--- frontend/devtaskhub-frontend/vite.config.ts
+++ frontend/devtaskhub-frontend/vite.config.ts
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-    css: true
-  }
+  plugins: [react()]
 });
